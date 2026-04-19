ffbfbf8121fd4449de27d5833ea9735a7816210c
colewright1997-dev <colewright1997@gmail.com>
2026-04-19 11:50:40 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import WrightRanchApp from './WrightRanchApp'
+import WrightRanchApp from './WrightRanchApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
